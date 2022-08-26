c61021c6dce121ba9ea7d83211e700070d5846dc
kai <leonc4667@gmail.com>
2022-08-26 11:48:50 -0400

    hehe

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://kaikrmen:Noah29..@mernapp.pwlad.mongodb.net/noteappwithsockets?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "";
